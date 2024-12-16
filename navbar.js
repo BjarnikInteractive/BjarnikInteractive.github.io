4a0b19e0f73a51133cad5b6849c8b0e4f5bc4853
BjarnikInteractive <bjarnik.interactive@gmail.com>
2024-12-17 00:21:23 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Hamburger Menu und das ul-Element der Navbar holen
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.navbar ul');
 
+  // Event Listener für das Klicken auf das Hamburger-Symbol
   hamburger.addEventListener('click', function() {
+    // Toggle 'active'-Klasse, um das Menü zu öffnen/schließen
     menu.classList.toggle('active');
   });
 });
