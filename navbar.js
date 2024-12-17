8163c668ae481c9f2051f1da82936583f855bd19
BjarnikInteractive <bjarnik.interactive@gmail.com>
2024-12-17 05:16:53 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,8 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navUl = document.querySelector('.navbar ul');
 
-hamburger.addEventListener('click', () => {
-  console.log('Hamburger wurde geklickt');
-  console.log(navUl); // Überprüfen, ob das ul-Element gefunden wird
-  navUl.classList.toggle('active');
-});
+if (hamburger && navUl) { // Überprüfung, ob die Elemente gefunden wurden
+  hamburger.addEventListener('click', () => {
+    console.log('Hamburger wurde geklickt');
+    console.log(navUl); // Ausgabe des ul-Elements in der Konsole
+    navUl.classList.toggle('active');
+  });
+} else {
+  console.error("Hamburger oder Navigations-UL nicht gefunden!");
+}
