1e13eea51657fcae530d1d2a87fd430f639b75cd
BjarnikInteractive <bjarnik.interactive@gmail.com>
2024-12-17 05:00:57 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,4 +1,8 @@
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('active'); // Toggle die "active"-Klasse zum Anzeigen/Ausblenden des Menüs
+// Hamburger Menü klicken
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const navLinks = document.getElementById('nav-links');
+
+// Toggle der 'active' Klasse bei Klick
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
